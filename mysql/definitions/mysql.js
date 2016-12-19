5b6b8749325e45595052c04a2dbc97560ae3fc65
PatchwerkQWER <PatchwerkQWER@users.noreply.github.com>
2016-12-19 01:10:02 +0100

    Small fix
    Loaded data from config + missing database

--- mysql/definitions/mysql.js
+++ mysql/definitions/mysql.js
@@ -1,8 +1,12 @@
 var mysql = require('mysql');
-var pool  = mysql.createPool({ host: 'example.org', user: 'bob', password: 'secret' });
+var pool  = mysql.createPool({ 
+	host: CONFIG('mysql-host'),
+	user: CONFIG('mysql-user'),
+	password: CONFIG('mysql-password'),
+	database: CONFIG('mysql-database')
+});
 
 // override the framework prototype
-// use CONFIG files for connection string
 F.database = function(callback) {
 	return pool.getConnection(callback);
 };
